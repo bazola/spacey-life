c4862f84f26df8a2926e6c22a08a8cbf0bf16b83
bazola <bazzybazz@gmail.com>
2015-12-21 08:01:43 -0600

    initial commit

---
+++ desktop/src/com/bazola/spaceylife/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.bazola.spaceylife.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.bazola.spaceylife.LibGDXGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new LibGDXGame(), config);
+	}
+}
---
+++ ios/src/com/bazola/spaceylife/IOSLauncher.java
@@ -0,0 +1,22 @@
+package com.bazola.spaceylife;
+
+import org.robovm.apple.foundation.NSAutoreleasePool;
+import org.robovm.apple.uikit.UIApplication;
+
+import com.badlogic.gdx.backends.iosrobovm.IOSApplication;
+import com.badlogic.gdx.backends.iosrobovm.IOSApplicationConfiguration;
+import com.bazola.spaceylife.LibGDXGame;
+
+public class IOSLauncher extends IOSApplication.Delegate {
+    @Override
+    protected IOSApplication createApplication() {
+        IOSApplicationConfiguration config = new IOSApplicationConfiguration();
+        return new IOSApplication(new LibGDXGame(), config);
+    }
+
+    public static void main(String[] argv) {
+        NSAutoreleasePool pool = new NSAutoreleasePool();
+        UIApplication.main(argv, null, IOSLauncher.class);
+        pool.close();
+    }
+}
