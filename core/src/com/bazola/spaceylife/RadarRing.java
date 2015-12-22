2e8a4213c3b97500a6df9429cffc19dba8c29803
bazola <bazzybazz@gmail.com>
2015-12-21 21:58:07 -0600

    remove ring at the end of its actions

--- core/src/com/bazola/spaceylife/RadarRing.java
+++ core/src/com/bazola/spaceylife/RadarRing.java
@@ -2,6 +2,7 @@ package com.bazola.spaceylife;
 
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.scenes.scene2d.actions.Actions;
+import com.badlogic.gdx.scenes.scene2d.actions.SequenceAction;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
 
 public class RadarRing extends Image {
@@ -13,7 +14,17 @@ public class RadarRing extends Image {
 		this.setSize(size, size);
 		this.setOrigin(size / 2, size / 2);
 		
+		//add one action and then the sequence
+		//so that the first two happen at the same time
 		this.addAction(Actions.scaleBy(50, 50, 2f));
-		this.addAction(Actions.fadeOut(2f));
+		SequenceAction sequence = new SequenceAction();
+		sequence.addAction(Actions.fadeOut(2f));
+		sequence.addAction(Actions.run(new Runnable() {
+			@Override
+			public void run() {
+				RadarRing.this.remove();
+			}
+		}));
+		this.addAction(sequence);
 	}
 }
